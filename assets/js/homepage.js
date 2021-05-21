f2240e184c1fedaf6f2fedcef03d3690e4c50e10
PravTon <clintonsf@outlook.com>
2021-05-21 18:54:21 -0400

    added query based on languages

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -2,6 +2,7 @@ var userFormEl = document.querySelector("#user-form");
 var nameInputEl = document.querySelector("#username");
 var repoContainerEl = document.querySelector("#repos-container");
 var repoSearchTerm = document.querySelector("#repo-search-term");
+var languageButtonEl = document.querySelector("#language-buttons");
 
 var formSubmitHandler = function(event) {
     event.preventDefault();
@@ -52,7 +53,6 @@ var displayRepos = function(repos, searchTerm) {
     for (var i = 0; i < repos.length; i++) {
         //format repo name
         var repoName = repos[i].owner.login + "/" + repos[i].name;
-        console.log(repos[i].name);
 
         //create a container for each repo
         var repoEl = document.createElement("a");
@@ -86,5 +86,35 @@ var displayRepos = function(repos, searchTerm) {
 
     
 };
+
+//function for search by topic buttons
+var buttonClickHandler = function(event) {
+    var language = event.target.getAttribute("data-language");
+
+    if(language) {
+        getFeaturedRepos(language);
+    } 
+
+    //clear old content
+    repoContainerEl.textContent = "";
+
+};
+
+var getFeaturedRepos = function(language) {
+    var apiUrl = "https://api.github.com/search/repositories?q=" + language +"+is:featured&sort=help-wanted-issues";
+
+    fetch(apiUrl).then(function(response) {
+        if(response.ok) {
+            response.json().then(function(data) {
+                displayRepos(data.items, language);
+            })
+        } else {
+            alert("Error: Github User Not Found");
+        }
+    })
+};
+
+
+languageButtonEl.addEventListener("click", buttonClickHandler);
 
 userFormEl.addEventListener("submit", formSubmitHandler);
